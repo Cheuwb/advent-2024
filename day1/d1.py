99d3c5de106c631fb8ae3631216e4336a55f4e71
ghxstrk <x3mgamer1@gmail.com>
2024-12-04 00:18:09 -0500

    day1 part2

--- day1/d1.py
+++ day1/d1.py
@@ -17,3 +17,11 @@ with open('input', 'r') as file:
         sum_answer = sum_answer + abs((int(column1[i]) - int(column2[i])))
 
     print(sum_answer)
+
+    #part2
+    score2 = 0
+    for number in column1:
+        count_in_column2 = column2.count(number)
+        score2 += int(number) * count_in_column2
+    
+    print(score2)
